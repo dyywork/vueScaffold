4b02efbf59873a05c5d1bf8ad95533c1c42e4cdc
10850 <1085058441@qq.com>
2020-05-09 09:07:22 +0800

    打印测试

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import store from './store/store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import 'lib-flexible/flexible'
+import VConsole from 'vconsole'
 import { mergeBeforeEachHook, VKeepAliveChain } from 'v-keep-alive-chain'
+var va = new VConsole()
 Vue.use(VKeepAliveChain, {
   key: 'cacheTo', // 可选的 默认为cacheTo
 })
@@ -15,6 +17,7 @@ router.beforeEach(mergeBeforeEachHook())
 router.beforeEach(mergeBeforeEachHook((to, from, next) => {
   next()
 }))
+
 Vue.config.productionTip = false
 Vue.use(Vant)
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,51 +1,104 @@
 <template>
   <div class="login">
-    <van-cell-group>
-      <van-field
-        v-model="username"
-        required
-        clearable
-        label="用户名"
-        right-icon="question-o"
-        placeholder="请输入用户名"
-        @click-right-icon="$toast('question')"
-      />
-
-      <van-field
-        v-model="password"
-        type="password"
-        label="密码"
-        placeholder="请输入密码"
-        required
-      />
-    </van-cell-group>
-    <van-button type="info" class="button" size="large" @click="login">登录</van-button>
+    {{username}}
+    <van-list>
+      <van-cell v-for="item in list" :title="item.name"></van-cell>
+    </van-list>
+    <van-button @click="login">ceshi</van-button>
+    <van-button @click="stop">体制</van-button>
   </div>
 </template>
 
 <script>
-import { Dialog } from 'vant'
 export default {
   name: 'login',
   data () {
     return {
       username: '',
-      password: ''
+      password: '',
+      list: [],
     }
+  },
+  created () {
+
+  },
+  mounted () {
+    // 获取蓝牙状态
+    document.addEventListener('plusready', this.onPlusReady, false);
   },
   methods: {
     login () {
-      if (this.username && this.password) {
-        this.$router.push('/tabs')
-      } else {
-        Dialog.alert({
-          message: '请输入用户名密码！'
-        }).then(() => {
-          // on close
-        })
-      }
-    }
-  }
+      plus.bluetooth.getBluetoothDevices({
+        success: function (e) {
+          var devices = e.devices;
+          this.list = [];
+          this.list = devices
+          this.username = JSON.stringify(devices);
+          console.log('get devices success: ' + JSON.stringify(devices));
+        },
+        fail: function (e) {
+          console.log('get devices failed: ' + JSON.stringify(e));
+        },
+      });
+    },
+    onPlusReady () {
+      console.log('plusready');
+      console.log(window)
+      window.plus.bluetooth.openBluetoothAdapter({
+        success: (e) => {
+          this.getBluetoothDevices();
+          /* console.log('open success: ' + JSON.stringify(e));
+          window.plus.bluetooth.getBluetoothAdapterState({
+            success: (e) => {
+              console.log('state success: ' + JSON.stringify(e));
+            },
+            fail: (e) => {
+              console.log('state failed: ' + JSON.stringify(e));
+            },
+          }) */
+        },
+        fail: (e) => {
+          console.log('open failed: ' + JSON.stringify(e));
+        },
+      });
+    },
+    getBluetoothDevices () {
+      plus.bluetooth.startBluetoothDevicesDiscovery({
+        success: function (e) {
+          plus.bluetooth.onBluetoothDeviceFound(function (e) {
+            var devices = e.devices;
+            console.log('device found: ' + devices);
+            for (var i in devices) {
+              console.log(i + ': ' + JSON.stringify(devices[i]));
+            }
+          });
+          console.log('start discovery success: ' + JSON.stringify(e));
+        },
+        fail: function (e) {
+          console.log('start discovery failed: ' + JSON.stringify(e));
+        },
+      });
+      /* window.plus.bluetooth.getBluetoothDevices({
+        success: function (e) {
+          var devices = e.devices;
+          console.log('get devices success: ' + JSON.stringify(e));
+        },
+        fail: function (e) {
+          console.log('get devices failed: ' + JSON.stringify(e));
+        },
+      }); */
+    },
+    stop () {
+      plus.bluetooth.closeBluetoothAdapter({
+        success: function (e) {
+          console.log('close success: ' + JSON.stringify(e));
+        },
+        fail: function (e) {
+          console.log('close failed: ' + JSON.stringify(e));
+        },
+      });
+    },
+  },
 }
 </script>
 
